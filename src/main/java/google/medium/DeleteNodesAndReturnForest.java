b60715d323de9184d9d637b66d54d96e4d5fe5a3
zcoderz <usmanqd@gmail.com>
2021-01-21 14:40:57 -0600

    adding in some more solutions.

---
+++ src/main/java/google/medium/DeleteNodesAndReturnForest.java
@@ -0,0 +1,93 @@
+package google.medium;
+
+
+import utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 1110. Delete Nodes And Return Forest
+ * Given the root of a binary tree, each node in the tree has a distinct value.
+ *
+ * After deleting all nodes with a value in to_delete, we are left with a forest (a disjoint union of trees).
+ *
+ * Return the roots of the trees in the remaining forest.  You may return the result in any order.
+ *
+ * Example 1:
+ *
+ *
+ *
+ * Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+ * Output: [[1,2,null,4],[6],[7]]
+ *
+ */
+public class DeleteNodesAndReturnForest {
+
+    List<TreeNode> roots = new ArrayList<>();
+
+    /**
+     * track the numbers to be deleted in a set
+     * this is simple. traverse tree before recursion stack returns delete the left and or right child if they are
+     * in the set to be deleted.
+     * to ensure that the set of items that are to be deleted get trimmed after every delete, remove the deleted
+     * item from the set
+     * @param root
+     * @param to_delete
+     * @return
+     */
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Set<Integer> set = new HashSet<>();
+        for (int v : to_delete) {
+            set.add(v);
+        }
+
+        //to do figure out how to handle delete of root
+        traverseTree(root, set);
+        if (set.contains(root.val)) {
+            if (root.left != null) {
+                roots.add(root.left);
+            }
+            if (root.right != null) {
+                roots.add(root.right);
+            }
+        } else {
+            roots.add(root);
+        }
+        return roots;
+    }
+
+    void traverseTree(TreeNode node, Set<Integer> toDelete) {
+        if(node == null) {
+            return;
+        }
+
+        traverseTree(node.left, toDelete);
+        traverseTree(node.right, toDelete);
+
+        if ((node.left != null) && (toDelete.contains(node.left.val))) {
+            toDelete.remove(node.left.val);
+            if (node.left.left != null) {
+                roots.add(node.left.left);
+            }
+            if (node.left.right != null) {
+                roots.add(node.left.right);
+            }
+            node.left = null;
+        }
+
+        if ((node.right != null) && (toDelete.contains(node.right.val))) {
+            toDelete.remove(node.right.val);
+            if (node.right.left != null) {
+                roots.add(node.right.left);
+            }
+            if (node.right.right != null) {
+                roots.add(node.right.right);
+            }
+            node.right = null;
+        }
+    }
+
+}
---
+++ src/main/java/google/medium/ExpressiveWords.java
@@ -0,0 +1,82 @@
+package google.medium;
+
+
+import java.util.Arrays;
+
+/**
+ * 809. Expressive Words
+ *
+ * Sometimes people repeat letters to represent extra feeling, such as "hello" -> "heeellooo", "hi" -> "hiiii".
+ * In these strings like "heeellooo", we have groups of adjacent letters that are all the same:  "h", "eee", "ll", "ooo".
+ *
+ * For some given string S, a query word is stretchy if it can be made to be equal to S by any number of applications
+ * of the following extension operation: choose a group consisting of characters c, and add some number of
+ * characters c to the group so that the size of the group is 3 or more.
+ *
+ * For example, starting with "hello", we could do an extension on the group "o" to get "hellooo",
+ * but we cannot get "helloo" since the group "oo" has size less than 3.  Also, we could do another extension
+ * like "ll" -> "lllll" to get "helllllooo".  If S = "helllllooo", then the query word "hello" would be stretchy
+ * because of these two extension operations: query = "hello" -> "hellooo" -> "helllllooo" = S.
+ *
+ * Given a list of query words, return the number of words that are stretchy.
+ *
+ *
+ *
+ * Example:
+ * Input:
+ * S = "heeellooo"
+ * words = ["hello", "hi", "helo"]
+ * Output: 1
+ * Explanation:
+ * We can extend "e" and "o" in the word "hello" to get "heeellooo".
+ * We can't extend "helo" to get "heeellooo" because the group "ll" is not size 3 or more.
+ */
+public class ExpressiveWords {
+
+    public static void main(String [] args) {
+        String str = "aaabbbaaa";
+        String [] words = {"aba"};
+        ExpressiveWords ew = new ExpressiveWords();
+        int ct = ew.expressiveWords(str, words);
+        System.out.println(ct);
+    }
+
+    public int expressiveWords(String str, String[] words) {
+        int strLen = str.length();
+        int matches = 0;
+        for (String word: words) {
+            int strIndex = 0;
+            int wordIndex = 0;
+            int wordLen = word.length();
+            char wChar;
+            char sChar;
+            int match = 1;
+            while (wordIndex < wordLen && strIndex < strLen) {
+                if (str.charAt(strIndex) != word.charAt(wordIndex)) {
+                    match =0;
+                    break;
+                }
+                int strCt = 1;
+                sChar = str.charAt(strIndex++);
+                while (strIndex < strLen && str.charAt(strIndex) == sChar) {
+                    strIndex++;
+                    strCt++;
+                }
+                int wCt = 1;
+                wChar = word.charAt(wordIndex++);
+                while (wordIndex < wordLen && word.charAt(wordIndex) == wChar) {
+                    wordIndex++;
+                    wCt++;
+                }
+                if ( (wCt != strCt) && ((strCt < wCt) || (strCt < 3))) {
+                    match =0;
+                    break;
+                }
+            }
+            if ((strIndex == strLen) && (wordIndex == wordLen)) {
+                matches += match;
+            }
+        }
+        return matches;
+    }
+}

6acd74cac2f49aa237625464ca6e1fb5884034a4
zcoderz <usmanqd@gmail.com>
2021-02-03 19:27:49 -0600

    new code.....

--- src/main/java/design/FileSystem.java
+++ src/main/java/design/FileSystem.java
@@ -10,8 +10,41 @@ import java.util.List;
 
 
 /**
+ * 588. Design In-Memory File System
+ * Design an in-memory file system to simulate the following functions:
+ *
+ * ls: Given a path in string format. If it is a file path, return a list that only contains this file's name.
+ * If it is a directory path, return the list of file and directory names in this directory.
+ * Your output (file and directory names together) should in lexicographic order.
+ *
+ * mkdir: Given a directory path that does not exist, you should make a new directory according to the path.
+ * If the middle directories in the path don't exist either, you should create them as well.
+ * This function has void return type.
+ *
+ * addContentToFile: Given a file path and file content in string format.
+ * If the file doesn't exist, you need to create that file containing given content.
+ * If the file already exists, you need to append given content to original content. This function has void return type.
+ *
+ * readContentFromFile: Given a file path, return its content in string format.
+ *
+ *
+ *
+ * Example:
+ *
+ * Input:
+ * ["FileSystem","ls","mkdir","addContentToFile","ls","readContentFromFile"]
+ * [[],["/"],["/a/b/c"],["/a/b/c/d","hello"],["/"],["/a/b/c/d"]]
+ *
+ * Output:
+ * [null,[],null,null,["a"],"hello"]
+ *
+ * Explanation:
+ *
+ *
  * Simple set of methods to implement in memory file system the code assumes that the paths being passed are
  * syntactically correct in production code there should be piece that validates the paths for correctness
+ *
+ * IMP-1 : nice design question to practice
  */
 public class FileSystem {
 
--- src/main/java/design/Vector2D.java
+++ src/main/java/design/Vector2D.java
@@ -2,6 +2,25 @@ package design;
 
 import java.util.NoSuchElementException;
 
+/**
+ * 251. Flatten 2D Vector
+ * Design and implement an iterator to flatten a 2d vector. It should support the following operations: next and hasNext.
+ *
+ *
+ *
+ * Example:
+ *
+ * Vector2D iterator = new Vector2D([[1,2],[3],[4]]);
+ *
+ * iterator.next(); // return 1
+ * iterator.next(); // return 2
+ * iterator.next(); // return 3
+ * iterator.hasNext(); // return true
+ * iterator.hasNext(); // return true
+ * iterator.next(); // return 4
+ * iterator.hasNext(); // return false
+ *
+ */
 public class Vector2D {
     int row, col;
     int[][] v;

b07661576d3b2b00f4dd976a4196d5b27a62dbbb
zcoderz <usmanqd@gmail.com>
2021-02-09 19:08:35 -0600

    new code.....

--- src/main/java/recursion/SwapNodes.java
+++ src/main/java/recursion/SwapNodes.java
@@ -2,9 +2,30 @@ package recursion;
 
 import utils.ListNode;
 
+/**
+ * 24. Swap Nodes in Pairs
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4]
+ * Output: [2,1,4,3]
+ * Example 2:
+ *
+ * Input: head = []
+ * Output: []
+ *
+ */
 public class SwapNodes {
 
-
+    /**
+     * recursively swap in pairs
+     * @param head
+     * @return
+     */
     public ListNode swapPairs(ListNode head) {
         if (head == null) {
             return null;
--- src/main/java/recursion/UniqueBinarySearchTrees.java
+++ src/main/java/recursion/UniqueBinarySearchTrees.java
@@ -7,13 +7,29 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * 95. Unique Binary Search Trees II
+ * Given an integer n, return all the structurally unique BST's (binary search trees),
+ * which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: n = 3
+ * Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+ * Example 2:
+ *
+ * Input: n = 1
+ * Output: [[1]]
+ *
+ */
 public class UniqueBinarySearchTrees {
 
     public static void main(String []args) {
         UniqueBinarySearchTrees ubst = new UniqueBinarySearchTrees();
         List<TreeNode> nodes = ubst.generateTrees(2);
         System.out.println(nodes.size());
-
     }
 
 
@@ -24,6 +40,13 @@ public class UniqueBinarySearchTrees {
         return generateTreesRec(1, n);
     }
 
+    /**
+     * clever idea - number of tree combinations is a cross product of left and right children
+     * so recuse for left and right partitions, do a double for loop to process each combination
+     * @param start
+     * @param end
+     * @return
+     */
     public List<TreeNode>  generateTreesRec(int start, int end) {
         if (start > end) {
             List<TreeNode> treeNodes = new ArrayList<>();

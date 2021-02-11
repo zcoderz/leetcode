4b15ac5498502fffe7bd13eb7a134059a0442b83
zcoderz <usmanqd@gmail.com>
2021-02-11 16:07:35 -0600

    new solutions...

--- src/main/java/sort/CountSmallerToSelf.java
+++ src/main/java/sort/CountSmallerToSelf.java
@@ -1,115 +1,91 @@
 package sort;
 
+import utils.Pair;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 315. Count of Smaller Numbers After Self
+ * You are given an integer array nums and you have to return a new counts array.
+ * The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [5,2,6,1]
+ * Output: [2,1,1,0]
+ * Explanation:
+ * To the right of 5 there are 2 smaller elements (2 and 1).
+ * To the right of 2 there is only 1 smaller element (1).
+ * To the right of 6 there is 1 smaller element (1).
+ * To the right of 1 there is 0 smaller element.
+ *
+ * https://leetcode.com/explore/interview/card/top-interview-questions-hard/118/trees-and-graphs/851/discuss/76584/Mergesort-solution
+ *
+ * IMP-1 : Very clever use of merge sort. Can be applied to other problems. Good to practice..
+ */
 public class CountSmallerToSelf {
 
     public static void main(String[] args) {
         CountSmallerToSelf ms = new CountSmallerToSelf();
-        int[] nums = {10, 1, 2, 5, 7, 3, 4, 44, 99};
-        ms.mergeSort(nums);
+        //int[] nums = {10, 1, 2, 5, 7, 3, 4, 44, 99};
+        int[] nums = {2,5,3,1};
+        List<Integer> count =  ms.countSmaller(nums);
+        for (Integer num : count) {
+            System.out.println(num);
+        }
     }
 
-    public List<Integer> mergeSort(int[] nums) {
-        int[][] vals = new int[nums.length][2];
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        Pair<Integer, Integer>[] arr = new Pair [nums.length];
+        Integer[] smaller = new Integer[nums.length];
+        Arrays.fill(smaller, 0);
         for (int i = 0; i < nums.length; i++) {
-            vals[i][0] = nums[i];
-            vals[i][1] = i;
+            arr[i] = new Pair<>(i, nums[i]);
         }
-
-        int[] count = new int[nums.length];
-        int[][] aux = new int[nums.length][2];
-        recurseMerge(0, nums.length - 1, vals, aux, count);
-
-        List<Integer> arr = new ArrayList<>();
-        for (int i = 0; i < count.length; i++) {
-            arr.add(count[i]);
-            System.out.println(count[i]);
-        }
-        return arr;
+        mergeSort(arr, smaller);
+        res.addAll(Arrays.asList(smaller));
+        return res;
     }
 
-    void recurseMerge(int low, int high, int[][] vals, int[][] aux, int[] count) {
-        if (low >= high) {
-            return;
+    /**
+     * Very clever solution using merge sort originally found by Stefan Pochmann
+     *
+     * @param arr
+     * @param smaller
+     * @return
+     */
+    private Pair<Integer, Integer>[] mergeSort(Pair<Integer, Integer>[] arr, Integer[] smaller) {
+        if (arr.length <= 1) {
+            return arr;
         }
-
-        int mid = (low + high) / 2;
-        recurseMerge(mid + 1, high, vals, aux, count);
-        recurseMerge(low, mid, vals, aux, count);
-
-        //if (num[mid] > num[mid+1]) return;
-        merge(low, mid, high, vals, aux, count);
-    }
-
-    void merge(int low, int mid, int high, int[][] num, int[][] aux, int[] count) {
-        for (int i = low; i <= high; i++) {
-            aux[i] = num[i];
-        }
-
-        int i = low;
-        int j = mid + 1;
-        int k = low;
-
-        while (k <= high) {
-
-            if (i > mid) {
-                num[k++] = aux[j++];
-            } else if (j > high) {
-                count[aux[i][1]] += j - (mid + 1);
-                num[k++] = aux[i++];
-
-            } else if (aux[i][0] > aux[j][0]) {
-                num[k++] = aux[j++];
+        int mid = arr.length / 2;
+        Pair<Integer, Integer>[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid), smaller);
+        Pair<Integer, Integer>[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length), smaller);
+        for (int i = 0, j = 0; i < left.length || j < right.length;) {
+            //pick the smaller element from the left or right array
+            if (j == right.length || i < left.length && left[i].second <= right[j].second) {
+                //each time we choose an item from the left array. we want to know how many
+                //numbers from array right are moved before this number.
+                arr[i + j] = left[i];
+                smaller[left[i].first] += j;
+                i++;
             } else {
-                count[aux[i][1]] += j - (mid + 1);
-                num[k++] = aux[i++];
+                //no action on count when picking item from the right array since the question only asks
+                //how many items to the right are less than the given item
+                arr[i + j] = right[j];
+                j++;
             }
-
-
         }
-
+        return arr;   
     }
+
 
 }
-
-//https://leetcode.com/explore/interview/card/top-interview-questions-hard/118/trees-and-graphs/851/discuss/76584/Mergesort-solution
-
-//class Solution {
-//    public List<Integer> countSmaller(int[] nums) {
-//        int n = nums.length;
-//        int[][] arr = new int[n][2];
-//        int[][] aux = new int[n][2];
-//        for (int i = 0; i < n; i++) {
-//            arr[i] = new int[] {nums[i], i};
-//        }
-//        int[] count = new int[n];
-//        sort(arr, count, 0, n - 1, aux);
-//        List<Integer> list = new ArrayList();
-//        for (int i = 0; i < n; i++){
-//            list.add(count[i]);
-//        }
-//        return list;
-//    }
-//
-//    private void sort(int[][] arr, int[] count, int lo, int hi, int[][] aux) {
-//        if (lo >= hi) return;
-//        int mid = lo + (hi - lo) / 2;
-//        sort(arr, count, lo, mid, aux);
-//        sort(arr, count, mid + 1, hi, aux);
-//        for (int i = lo; i <= hi; i++) {
-//            aux[i] = arr[i];
-//        }
-//        int i = lo, j = mid + 1;
-//        for (int k = lo; k <= hi; k++) {
-//            if (i == mid + 1) { arr[k] = aux[j++]; }
-//            else if (j == hi + 1 || aux[i][0] <= aux[j][0]) {
-//                count[aux[i][1]] += j - (mid + 1);
-//                arr[k] = aux[i++];
-//            } else {
-//                arr[k] = aux[j++];
-//            }
-//        }
-//    }
-//}
--- src/main/java/sort/KthLargestElement.java
+++ src/main/java/sort/KthLargestElement.java
@@ -2,6 +2,26 @@ package sort;
 
 import java.util.Random;
 
+/**
+ * 215. Kth Largest Element in an Array
+ * Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ *
+ * Example 1:
+ *
+ * Input: [3,2,1,5,6,4] and k = 2
+ * Output: 5
+ * Example 2:
+ *
+ * Input: [3,2,3,1,2,4,5,5,6] and k = 4
+ * Output: 4
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ array's length.
+ *
+ * IMP-3: Common interview questions are on this theme.
+ * The same question is also solved in the class : KthLargest via binary trees.
+ * Most efficient to find the slot via quick select
+ *
+ */
 public class KthLargestElement {
 
     Random random = new Random();
@@ -13,8 +33,13 @@ public class KthLargestElement {
         int val = kthLargestElement.findKthLargest(nums, k);
     }
 
+    /**
+     * find the (length of nums - k) smallest element, thats the same as the kth largest element
+     * @param nums
+     * @param k
+     * @return
+     */
     public int findKthLargest(int[] nums, int k) {
-
         return searchKSmallest(0, nums.length - 1, nums.length - k, nums);
     }
 

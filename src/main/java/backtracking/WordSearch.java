e4374096c3805aa8ad4b9fd2c96b5af8184e3760
zcoderz <usmanqd@gmail.com>
2021-03-31 18:35:45 -0500

    new solutions...

--- src/main/java/backtracking/WordSearch.java
+++ src/main/java/backtracking/WordSearch.java
@@ -24,14 +24,22 @@ import java.util.Set;
  */
 public class WordSearch {
 
+    public static void main(String [] args) {
+        WordSearch ws = new WordSearch();
+        char [][] arr = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word = "ABCCED";
+        boolean res = ws.exist(arr, word);
+        System.out.println(res);
+    }
+    //right, left, up, down
+    int [] rowMoves = {0, 0, -1, 1};
+    int [] colMoves = {1, -1, 0, 0};
+
     public boolean exist(char[][] board, String word) {
-        char ch = word.charAt(0);
         for (int i = 0 ; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j ++) {
-                if (board[i][j] == ch) {
-                    if (traverse(i, j, board, word, 1) == true) {
-                        return  true;
-                    }
+                if (traverse(i, j, board, word, 0)) {
+                    return  true;
                 }
             }
         }
@@ -39,35 +47,24 @@ public class WordSearch {
     }
 
     public boolean traverse(int row, int col, char [][] board, String word, int wordIndex) {
-        if (wordIndex >= word.length()) return true;
-
+        if (wordIndex >= word.length()) {
+            return true;
+        }
         char ch = word.charAt(wordIndex);
-        int rightCol = col+1;
-        int leftCol = col-1;
-        int rowUp = row - 1;
-        int rowDown = row + 1;
-        if (leftCol < 0 || rowUp <0 || rightCol >= board[0].length || rowDown >= board.length ||
-                board[row][col] != word.charAt(wordIndex)) {
+        if (row < 0 || col <0 || col == board[0].length || row == board.length ||
+                board[row][col] != ch) {
             return false;
         }
-        //right, left, up, down
-        int [] rowMoves = {0, 0, -1, 1};
-        int [] colMoves = {1, -1, 0, 0};
-
         board[row][col]='#';
-
         for (int i =0 ; i < 4; i++) {
             int nCol = col + colMoves[i];
-            int nRow = row = rowMoves[i];
-            if (traverse(nRow, nCol, board, word, wordIndex + 1) == true) {
-                //return without cleanup
+            int nRow = row + rowMoves[i];
+            if (traverse(nRow, nCol, board, word, wordIndex + 1)) {
+                board[row][col] = ch;
                 return  true;
             }
         }
-
-        board[row][col] = word.charAt(wordIndex);
-        
+        board[row][col] = ch;
         return false;
-
     }
 }
--- src/main/java/google/medium/SentenceSimilarity.java
+++ src/main/java/google/medium/SentenceSimilarity.java
@@ -7,19 +7,19 @@ import java.util.*;
 /**
  * 737. Sentence Similarity II Given two sentences words1, words2 (each represented as an array of strings), and a list
  * of similar word pairs pairs, determine if two sentences are similar.
- * <p>
+ *
  * For example, words1 = ["great", "acting", "skills"] and words2 = ["fine", "drama", "talent"] are similar, if the
  * similar word pairs are pairs = [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]].
- * <p>
+ *
  * Note that the similarity relation is transitive. For example, if "great" and "good" are similar, and "fine" and
  * "good" are similar, then "great" and "fine" are similar.
- * <p>
+ *
  * Similarity is also symmetric. For example, "great" and "fine" being similar is the same as "fine" and "great" being
  * similar.
- * <p>
+ *
  * Also, a word is always similar with itself. For example, the sentences words1 = ["great"], words2 = ["great"], pairs
  * = [] are similar, even though there are no specified similar word pairs.
- * <p>
+ *
  * Finally, sentences can only be similar if they have the same number of words. So a sentence like words1 = ["great"]
  * can never be similar to words2 = ["doubleplus","good"].
  *

d0a6db136a8b543d91d8618c89f3409500890fad
zcoderz <usmanqd@gmail.com>
2021-04-01 20:53:30 -0500

    new solutions...

--- src/main/java/trees/wordladder/WordLadderEfficient.java
+++ src/main/java/trees/wordladder/WordLadderEfficient.java
@@ -7,6 +7,38 @@ import java.util.*;
 /**
  * * 127. Word Ladder
  *
+ * A transformation sequence from word beginWord to word endWord using a dictionary wordList is
+ * a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+ *
+ * Every adjacent pair of words differs by a single letter.
+ * Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+ * sk == endWord
+ * Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+ * Output: 5
+ * Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+ * Example 2:
+ *
+ * Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+ * Output: 0
+ * Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= beginWord.length <= 10
+ * endWord.length == beginWord.length
+ * 1 <= wordList.length <= 5000
+ * wordList[i].length == beginWord.length
+ * beginWord, endWord, and wordList[i] consist of lowercase English letters.
+ * beginWord != endWord
+ * All the words in wordList are unique.
+ *
  * See WordLadder for a simpler solution.
  * A very interesting approach the word ladder problem. Work at the problem from both bottom up and top down via two
  * directional BFS
@@ -52,6 +84,10 @@ public class WordLadderEfficient {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Map<String, List<String>> wordMappings = new HashMap<>();
         boolean matchEnd = false;
+        //words can connect if they are only one char apart at most - i,e hit can connect with hot and pit
+        //hence we can match by replacing each char in string one by one with * and creating mapping to original string
+        //and storing this mapping in a map with key as modified word and value as list of words that map the modified
+        //word
         for (String word : wordList) {
             for (int i = 0; i < word.length(); i++) {
                 String modWord = word.substring(0, i) + "*" + word.substring(i + 1);
